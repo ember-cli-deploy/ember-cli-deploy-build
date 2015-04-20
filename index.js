dfc8834d4eca2e2f1d700e1b2ae6531b81f08f79
Aaron Chambers <achambers@gmail.com>
2015-04-20 14:16:01 +0100

    Implemented the ember-cli-deploy `build` hook.
    This hook will build the project. It uses the ember-cli Builder, as opposed to the
    BuildTask, as the BuildTask requires the analytics object which we don't
    want to have to provide.

--- index.js
+++ index.js
@@ -1,6 +1,49 @@
 /* jshint node: true */
 'use strict';
 
+var chalk = require('chalk');
+
 module.exports = {
-  name: 'ember-cli-deploy-build'
+  name: 'ember-cli-deploy-build',
+
+  createDeployPlugin: function(options) {
+    return {
+      name: options.name,
+
+      build: function(context) {
+        var config  = context.config.rawConfig.build;
+        var ui      = context.ui;
+        var project = context.project;
+
+        var outputPath = 'dist';
+        var buildEnv   = config.buildEnv;
+
+        ui.startProgress(chalk.green('Building'), chalk.green('.'));
+
+        var Builder  = require('ember-cli/lib/models/builder');
+        var builder = new Builder({
+          ui: ui,
+          outputPath: outputPath,
+          environment: buildEnv,
+          project: project
+        });
+
+        return builder.build()
+          .finally(function() {
+            ui.stopProgress();
+            return builder.cleanup();
+          })
+          .then(function() {
+            ui.writeLine(chalk.green('Built project successfully. Stored in "' +
+              outputPath + '".'));
+          })
+          .catch(function(err) {
+            ui.writeLine(chalk.red('Build failed.'));
+            ui.writeError(err);
+
+            return 1;
+          });
+      }
+    }
+  }
 };
